f6950da2fc3d0a6353dc16afe5550ddc9ee7799b
velnova <alsdud151350@gmail.com>
2023-01-21 18:50:10 +0900

    FIRST COMMIT

---
+++ src/main/java/kr/co/velnova/githubactionspringboot/GithubActionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package kr.co.velnova.githubactionspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/velnova/githubactionspringboot/GithubActionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.velnova.githubactionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
