cbd40ad011e300ad7ef43cac18efbbc396e07c06
velnova <alsdud151350@gmail.com>
2023-01-21 18:52:44 +0900

    test

---
+++ src/test/java/kr/co/velnova/githubactionspringboot/UnitTest.java
@@ -0,0 +1,15 @@
+package kr.co.velnova.githubactionspringboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UnitTest {
+
+    @Test
+    void test() {
+        assertThat(true).isFalse();
+    }
+
+}
